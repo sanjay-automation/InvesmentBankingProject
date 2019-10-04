a9170ecab963e90b5a4fa33a6caa37960753b447
Sanjay <Sanjay@Sanjay-PC>
2019-10-04 16:21:59 +0530

    Open Kite Application

---
+++ InvestmentBanking/src/main/java/investmentbanking/corporateclientsolution/InvestmentBanking/OpenKite.java
@@ -0,0 +1,10 @@
+package investmentbanking.corporateclientsolution.InvestmentBanking;
+
+public class OpenKite {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
